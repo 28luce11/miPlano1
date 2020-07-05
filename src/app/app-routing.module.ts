716d11a23fd16d99174752df8aebb4f6871b9b0f
Luce <luce_tenis@hotmail.com>
2020-07-04 23:35:36 -0500

    carpetas basicas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './pages/home/home.component';
+
+
+
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '**', component: HomeComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class FeatureRoutingModule {}
